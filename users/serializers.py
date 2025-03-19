2246df6f5557951118cc5f5d7e28a59c8d10e3c5
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-03-19 17:49:49 +0200

    Create UserSerializer

---
+++ users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "password", "is_staff")
+        read_only = ("id", "is_staff")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"},
+                "label": _("Password"),
+            }
+        }
+
+    def create(self, validated_data):
+        """Create User with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update User with encrypted password."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
